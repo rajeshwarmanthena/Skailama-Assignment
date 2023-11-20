6961ba8b653e53725cf4ef3d78e94fe6e6d84c2f
neeru0713 <neerurani1307@gmail.com>
2023-11-21 00:43:19 +0530

    configration page frontend complete

--- src/components/Configuration.js
+++ src/components/Configuration.js
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useState } from "react";
 // import Sidenav from './Sidenav'
 import { FaSortDown, FaRegBell } from "react-icons/fa6";
+import { Input } from "./Input";
 import { SlHome } from "react-icons/sl";
 import united from "../united-kingdom.png";
 import { SlPencil } from "react-icons/sl";
 import { BiZoomIn } from "react-icons/bi";
-// import HorizontalLine from './HorizontalLine'; 
+// import HorizontalLine from './HorizontalLine';
 
 const Configuration = () => {
+  const [selectedCategory, setSelectedCategory] = useState();
+
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+  };
   return (
-    <div className="flex flex-col mx-12 gap-10">
-          <div className="flex mt-5 justify-between">
+    <div className="flex flex-col mx-12 gap-8">
+      <div className="flex mt-5 justify-between">
         <div className="flex gap-2">
           <a href="/home">
             <SlHome className="text-[#7e22ce] font-bold text-xl " />
@@ -30,16 +36,75 @@ const Configuration = () => {
           <img src={united} className="h-[30px] pl-2" />
           <FaRegBell className="p-1 text-3xl font-bold" />
         </div>
-         </div>
-         <p className='text-4xl text-[#7e22ce] font-bold'>Configuration</p>
-         <div className='flex text-md font-bold text-[#7e22ce] gap-10' >
-          <h1>General</h1>
-          <h1>Display</h1>
-          <h1>Advance</h1>
-         </div>
-         {/* <HorizontalLine className='w-[80%] text-gray'/> */}
-    </div>
-  )
-}
+      </div>
+      <p className="text-4xl text-[#7e22ce] font-bold">Configuration</p>
+      <div className="flex flex-col">
+        <div className="flex text-md font-bold  gap-4">
+          <h2
+            onClick={() => {
+              setSelectedCategory("general");
+            }}
+            class={`${
+              selectedCategory === "general"
+                ? "z-20 border-b-4 border-[#7e22ce] text-[#7e22ce]"
+                : ""
+            }`}
+          >
+            General
+          </h2>
 
-export default Configuration
+          <h2
+            onClick={() => {
+              setSelectedCategory("display");
+            }}
+            class={`${
+              selectedCategory === "display"
+                ? "z-20 border-b-4 border-[#7e22ce] text-[#7e22ce]"
+                : ""
+            }`}
+          >
+            Display
+          </h2>
+          <h2
+            onClick={() => {
+              setSelectedCategory("advance");
+            }}
+            class={`${
+              selectedCategory === "advance"
+                ? "z-20 border-b-4 border-[#7e22ce] text-[#7e22ce]"
+                : ""
+            }`}
+          >
+            Advance
+          </h2>
+        </div>
+        {/* <HorizontalLine className='w-[80%] text-gray'/> */}
+        <div className="border-t-4 border-gray-400 width-full mt-[-4px] rounded"></div>
+      </div>
+
+      {selectedCategory === "general" && (
+        <form className="flex flex-col gap-1">
+          <Input className="text-xl font-bold" label="Chatbot Name" />
+          <p className="text-md text-gray-400">
+            Lorem ipsuim dolor sit Lorem ipsuim dolor sit
+          </p>
+
+          <Input className="text-xl font-bold" label="Welcome Message" />
+          <p className="text-md text-gray-400">
+            Lorem ipsuim dolor sit Lorem ipsuim dolor sit
+          </p>
+          <Input className="text-xl font-bold" label="Input Placeholder" />
+          <p className="text-md text-gray-400">
+            Lorem ipsuim dolor sit Lorem ipsuim dolor sit
+          </p>
+        </form>
+      )}
+
+      {selectedCategory === "display" && <div>Display</div>}
+
+      {selectedCategory === "advance" && <div>Advance</div>}
+    </div>
+  );
+};
+
+export default Configuration;
